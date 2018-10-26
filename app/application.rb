e4ff8a2f12bea3e6562926aab3e1c47cf62cadac
Rebecca Strauss <rstrauss127@gmail.com>
2018-10-26 21:53:47 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,11 @@ class Application
   def call(env)
     resp = Rack::Response.new
 
-    resp.write "#{Time.now}"
+    if Time.now.hour > 11
+      resp.write "Good Afternoon!"
+    else
+      resp.write "Good Morning!"
+    end
     resp.finish
   end
 end
